c677968d03496941a449fafdea7f7d7b3588ba66
ruvaar <ra7285@student.uni-lj.si>
2025-12-11 20:36:58 +0100

    Integrate Docker support: add docker configuration to angular.json, create environment.docker.ts, and update package.json scripts.

--- FriFood/src/environments/environment.ts
+++ FriFood/src/environments/environment.ts
@@ -2,8 +2,9 @@ export const environment = {
   production: false,
 
   // Microservice URLs
-  authServiceUrl: 'http://localhost:8001',
-  userServiceUrl: 'http://localhost:8002',
-  orderServiceUrl: 'http://localhost:8003',
-  // in tako naprej
+  authServiceUrl: 'http://localhost:8005',
+  userServiceUrl: 'http://localhost:8004',
+  orderServiceUrl: 'http://localhost:8002',
+  offerServiceUrl: 'http://localhost:8001',
+  paymentServiceUrl: 'http://localhost:8003',
 };
