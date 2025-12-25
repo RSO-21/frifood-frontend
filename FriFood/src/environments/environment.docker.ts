0917b601d8cc1501400181134c81588f0e2a223f
edita <cedevita@ass.si>
2025-12-25 14:16:38 +0100

    docker env

--- FriFood/src/environments/environment.docker.ts
+++ FriFood/src/environments/environment.docker.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: false,
 
   // Container-to-container URLs (service discovery via Docker network)
-  authServiceUrl: 'http://auth-service:8000',
-  userServiceUrl: 'http://user-service:8000',
-  orderServiceUrl: 'http://order-service:8000',
-  offerServiceUrl: 'http://offer-service:8000',
-  paymentServiceUrl: 'http://payment-service:8000',
+  authServiceUrl: 'http://localhost:8005',
+  userServiceUrl: 'http://localhost:8004',
+  orderServiceUrl: 'http://localhost:8002',
+  offerServiceUrl: 'http://localhost:8001',
+  paymentServiceUrl: 'http://localhost:8003',
 };
