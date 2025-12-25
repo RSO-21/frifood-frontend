e59ae8a39b1c4c8e6c5eb4f0d389ef3d3532c12d
edita <cedevita@ass.si>
2025-12-25 18:43:41 +0100

    remove env from tracking

--- FriFood/src/environments/environment.prod.example.ts
+++ FriFood/src/environments/environment.prod.example.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: true,
-
+  GOOGLE_API_KEY: '',
   // Real deployed URLs
   authServiceUrl: '',
   userServiceUrl: '',
   orderServiceUrl: '',
+  offerServiceUrl: '',
+  paymentServiceUrl: '',
   // in tako naprej
 };
