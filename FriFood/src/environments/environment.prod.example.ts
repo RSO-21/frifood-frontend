bef0215eb5355b7a282c02a96c66eb5fc02a8881
ruvaar <ra7285@student.uni-lj.si>
2026-01-06 23:35:51 +0100

    implement environment separation for Docker builds and update API URLs in services

--- FriFood/src/environments/environment.prod.example.ts
+++ FriFood/src/environments/environment.prod.example.ts
@@ -1,12 +1,14 @@
 export const environment = {
   production: true,
-  GOOGLE_API_KEY: '',
-  // Real deployed URLs
-  authServiceUrl: '',
-  userServiceUrl: '',
-  orderServiceUrl: '',
-  offerServiceUrl: '',
-  paymentServiceUrl: '',
-  partnerServiceUrl: '',
-  // in tako naprej
-};
+  googleMapsApiKey: '',
+
+  // AKS prod ingress URLs (same as dev but without '-dev')
+  apiGatewayUrl: 'http://api.fri-food.68.210.120.104.nip.io',
+  authServiceUrl: 'http://auth.68.210.120.104.nip.io',
+  userServiceUrl: 'http://users.68.210.120.104.nip.io',
+  orderServiceUrl: 'http://orders.68.210.120.104.nip.io',
+  offerServiceUrl: 'http://offers.68.210.120.104.nip.io',
+  paymentServiceUrl: 'http://payments.68.210.120.104.nip.io',
+  partnerServiceUrl: 'http://partners.68.210.120.104.nip.io',
+  notificationServiceUrl: 'http://notifications.68.210.120.104.nip.io',
+} as const;
