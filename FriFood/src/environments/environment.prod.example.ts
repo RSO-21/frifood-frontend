3f62ef6d724fd58b834b9308340300a8e331679a
ruvaar <ra7285@student.uni-lj.si>
2026-01-10 18:28:38 +0100

    fix: disable prerendering in build process and add review service URLs to environment files

--- FriFood/src/environments/environment.prod.example.ts
+++ FriFood/src/environments/environment.prod.example.ts
@@ -3,12 +3,13 @@ export const environment = {
   googleMapsApiKey: '',
 
   // AKS prod ingress URLs (same as dev but without '-dev')
-  apiGatewayUrl: 'http://api.fri-food.68.210.120.104.nip.io',
-  authServiceUrl: 'http://auth.68.210.120.104.nip.io',
-  userServiceUrl: 'http://users.68.210.120.104.nip.io',
-  orderServiceUrl: 'http://orders.68.210.120.104.nip.io',
-  offerServiceUrl: 'http://offers.68.210.120.104.nip.io',
-  paymentServiceUrl: 'http://payments.68.210.120.104.nip.io',
-  partnerServiceUrl: 'http://partners.68.210.120.104.nip.io',
-  notificationServiceUrl: 'http://notifications.68.210.120.104.nip.io',
+  apiGatewayUrl: 'http://api.fri-food.4.165.160.2.nip.io',
+  authServiceUrl: 'http://auth.4.165.160.2.nip.io',
+  userServiceUrl: 'http://users.4.165.160.2.nip.io',
+  orderServiceUrl: 'http://orders.4.165.160.2.nip.io',
+  offerServiceUrl: 'http://offers.4.165.160.2.nip.io',
+  paymentServiceUrl: 'http://payments.4.165.160.2.nip.io',
+  partnerServiceUrl: 'http://partners.4.165.160.2.nip.io',
+  notificationServiceUrl: 'http://notifications.4.165.160.2.nip.io',
+  reviewServiceUrl: 'http://reviews.4.165.160.2.nip.io'
 } as const;
