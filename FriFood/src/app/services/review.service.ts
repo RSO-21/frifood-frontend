72405201c0a290ede5e6b14991952bbd02bec7e4
unknown <tina.postuvan@gmail.com>
2026-01-19 21:23:54 +0100

    fix

--- FriFood/src/app/services/review.service.ts
+++ FriFood/src/app/services/review.service.ts
@@ -12,13 +12,13 @@ export class ReviewService {
   private readonly API_URL_GATEWAY = environment.apiGatewayUrl;
 
   createReview(input: { order_id: number; user_id: string; rating: number; comment?: string }) {
-    return this.http.post(`${this.API_URL_GATEWAY}/reviews`, input);
+    return this.http.post(`${this.API_URL_GATEWAY}/reviews/reviews`, input);
   }
 
   getPartnersRatings(partnerIds: string[]) {
     const params = partnerIds.join(',');
     return this.http.get<Record<string, PartnerRating>>(
-      `${this.API_URL_GATEWAY}/partners/ratings`,
+      `${this.API_URL_GATEWAY}/reviews/partners/ratings`,
       {
         params: { partner_ids: params },
       }
